4064f186138f4d124187f6fe02d50faecf6f4ff3
Coleman Lai <colemancflai@yahoo.com>
2025-01-15 00:31:21 -0800

    Added sitemap and robots

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import Sitemap from 'vite-plugin-sitemap'
+const names = [
+  'origami',
+  'portfolio',
+  'portfolio/personal-website',
+]
+const dynamicRoutes = names.map(name => `/${name}`)
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap({ hostname: "https://www.colemanlai.com", readable: true, dynamicRoutes }),],
   base: '/',
   build: {
     outDir: 'dist',
