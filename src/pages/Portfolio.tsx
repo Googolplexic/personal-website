d71353197c3a50ae937619aa2814bc572f43cac4
Cole <52732344+Googolplexic@users.noreply.github.com>
2025-01-15 01:59:55 -0800

    Added incompleteness notice

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -7,6 +7,7 @@ function PortfolioGrid() {
     return (
         <div className="text-center">
             <h1 className="text-4xl font-bold mb-8">Portfolio</h1>
+            <p className="mb-8">This list is incompete. See my <a href="https://github.com/Googolplexic" target="_blank" rel="noopener noreferrer">GitHub</a> for more projects</p>  
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-8 md:mx-0">
                 {projects.map((project, index) => (
                     <Project
