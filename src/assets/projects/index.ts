3aff7f6fda810f326c62a0c958cd8ef9ecdc132d
Coleman Lai <colemancflai@yahoo.com>
2025-01-15 01:14:11 -0800

    added sort

--- src/assets/projects/index.ts
+++ src/assets/projects/index.ts
@@ -4,7 +4,8 @@ const modules = import.meta.glob('./**/index.ts', { eager: true });
 const projects: ProjectProps[] = Object.entries(modules)
     .filter(([path]) => !path.includes('/template/'))
     .map(([, module]) => (module as { default: ProjectProps }).default)
-    .filter(project => project !== undefined);
+    .filter(project => project !== undefined)
+    .sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
 
 export default projects;
 
