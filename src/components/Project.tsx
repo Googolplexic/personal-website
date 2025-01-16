3a5e575a4bdb8054d1d24f4e3ca6062390b73e3b
Coleman Lai <colemancflai@yahoo.com>
2025-01-16 00:03:37 -0800

    Adjusted sizing for project grid

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -17,7 +17,7 @@ export function Project(props: ProjectProps) {
     return (
         <div 
             onClick={handleClick}
-            className="cursor-pointer border-2 rounded-lg p-4 mb-4 dark:border-gray-700 hover:[box-shadow:0_0_15px_2px_rgba(0,0,0,0.2)] dark:hover:[box-shadow:0_0_15px_2px_rgba(255,255,255,0.2)] transition-shadow h-full flex justify-center flex-col"
+            className="cursor-pointer border-2 rounded-lg lg:p-12 p-6 mb-4 dark:border-gray-700 hover:[box-shadow:0_0_15px_2px_rgba(0,0,0,0.2)] dark:hover:[box-shadow:0_0_15px_2px_rgba(255,255,255,0.2)] transition-shadow h-full flex justify-center flex-col"
         >
             {props.images && props.images.length > 0 && (
                 <div className="flex gap-2 mb-4 mx-auto justify-center">
@@ -32,7 +32,7 @@ export function Project(props: ProjectProps) {
             <p className="text-sm text-gray-600 dark:text-gray-400 mb-2">
                 {props.startDate === props.endDate ? props.startDate : `${props.startDate} - ${props.endDate || 'Present'}`}
             </p>
-            <p className="mb-4">{props.summary}</p>
+            <p className="">{props.summary}</p>
             <ProjectTechnologies technologies={props.technologies} />
             <ProjectLinks project={props} />
         </div>
--- src/components/ProjectTechnologies.tsx
+++ src/components/ProjectTechnologies.tsx
@@ -5,9 +5,9 @@ interface ProjectTechnologiesProps {
 export function ProjectTechnologies({ technologies }: ProjectTechnologiesProps) {
     return (
         <div className="my-4">
-            <div className="flex flex-wrap gap-2 justify-center text-xs md:text-sm">
+            <div className="flex flex-wrap gap-2 justify-center  text-[0.7rem] sm:text-xs md:text-sm">
                 {technologies.map((tech, index) => (
-                    <span key={index} className="bg-gray-200 dark:bg-gray-700 px-2 py-1 rounded">
+                    <span key={index} className="bg-gray-200 dark:bg-gray-700 sm:px-2 px-1 sm:py-1 py-0 rounded">
                         {tech}
                     </span>
                 ))}
