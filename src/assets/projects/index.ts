fc448e742d54c3fa1e037631cba60bf505f4e48d
Coleman Lai <colemancflai@yahoo.com>
2025-01-15 23:44:08 -0800

    Better project date sorting

--- src/assets/projects/index.ts
+++ src/assets/projects/index.ts
@@ -1,11 +1,20 @@
 import { ProjectProps } from '../../types';
 
+const getMostRecentDate = (project: ProjectProps) => {
+    if (!project.endDate) return new Date(); 
+    return new Date(project.endDate);
+};
+
 const modules = import.meta.glob('./**/index.ts', { eager: true });
 const projects: ProjectProps[] = Object.entries(modules)
     .filter(([path]) => !path.includes('/template/'))
     .map(([, module]) => (module as { default: ProjectProps }).default)
     .filter(project => project !== undefined)
-    .sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+    .sort((a, b) => {
+        const dateA = getMostRecentDate(a);
+        const dateB = getMostRecentDate(b);
+        return dateB.getTime() - dateA.getTime();
+    });
 
 export default projects;
 
